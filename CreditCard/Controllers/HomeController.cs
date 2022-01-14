5e6889ad325568f19745aa4dd9cdc4ba007891fe
Missb123# <chinelsy@gmail.co.uk>
2022-01-14 14:20:52 +0100

    final commit

---
+++ CreditCard/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Braintree;
+using CreditCard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace CreditCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBraintreeService _braintreeService;
+        public HomeController(IBraintreeService braintreeService)
+        {
+            _braintreeService = braintreeService;
+        }
+
+        public IActionResult Index()
+        {
+            var gateway = _braintreeService.GetGateway();
+            var clientToken = gateway.ClientToken.Generate();  //Genarate a token
+            ViewBag.ClientToken = clientToken;
+
+            var data = new BookPurchaseVM
+            {
+                Id = 2,
+                Description = "Hellow man",
+                Author = "Me",
+                Thumbnail = "This is thumbnail",
+                Title = "This is title",
+                Price = "230",
+                Nonce = ""
+            };
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookPurchaseVM model)
+        {
+            var gateway = _braintreeService.GetGateway();
+            var request = new TransactionRequest
+            {
+                Amount = Convert.ToDecimal("250"),
+                PaymentMethodNonce = model.Nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View("Failure");
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CreditCard/Models/BookPurchaseVM.cs
@@ -0,0 +1,8 @@
+﻿namespace CreditCard.Models
+{
+    public class BookPurchaseVM : Book
+    {
+        public string Nonce { get; set; }
+        public bool? Success { get; set; }
+    }
+}
